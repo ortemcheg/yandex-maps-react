10ac1b79d6cef76e5e5fb179e1f298d771cf3a13
Tyoma Deev <tema@deev.ru>
2018-12-07 13:35:10 +0300

    encforced typechecking for props with PropTypes

--- YandexMap.js
+++ YandexMap.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 class YandexMap extends Component {
   constructor(props){
@@ -6,31 +7,37 @@ class YandexMap extends Component {
     this.state = {
       mapAPIIsReady: false,
       mapLoaded: false,
+      zoom: props.zoom,
       location: {
         lat: props.location.lat,
         lng: props.location.lng,
-        zoom: props.location.zoom
       }
     };
     this.mapRef = React.createRef();
     this.initAPI = this.initAPI.bind(this);
   }
   static defaultProps = {
+    zoom: 16,
     location: {
       lat: 55.773206,
       lng: 38.446328,
-      zoom: 16
     }
+  }
+  static propTypes = {
+    location: PropTypes.shape({
+      lat: PropTypes.number.isRequired,
+      lng: PropTypes.number.isRequired
+    }),
+    zoom: PropTypes.number.isRequired
   }
   componentDidMount() {
     this.ymaps = window.ymaps;
     if(!this.state.mapAPIIsReady) this.initAPI();
   }
   componentDidUpdate(){
-    if(!this.state.mapAPIIsReady) console.log('componentWillUpdate: Holly Molly! The API is still not ready!');
     this.map = new this.ymaps.Map(this.mapRef.current, {
       center: [this.state.location.lat, this.state.location.lng],
-      zoom: this.state.location.zoom
+      zoom: this.state.zoom
     });
   }
   initAPI(){
